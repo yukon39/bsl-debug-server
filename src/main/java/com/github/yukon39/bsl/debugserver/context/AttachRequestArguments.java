46b9d392e3936659e3a9bbfa13a74942f5584e10
Y. Goncharuk <y.goncharuk@centrofinans.ru>
2020-08-01 12:25:12 +0300

    feat: Add new attach parameters

--- src/main/java/com/github/yukon39/bsl/debugserver/context/AttachRequestArguments.java
+++ src/main/java/com/github/yukon39/bsl/debugserver/context/AttachRequestArguments.java
@@ -5,8 +5,11 @@ import com.github.yukon39.bsl.debugserver.utils.Resources;
 import lombok.Getter;
 
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.InvalidParameterException;
 import java.util.Map;
+import java.util.Objects;
 
 @Getter
 public class AttachRequestArguments {
@@ -14,7 +17,8 @@ public class AttachRequestArguments {
     private final URL debuggerURL;
     private final String infobaseAlias;
     private final char[] password;
-    private final String sessionId;
+    private final String cwd;
+    private final String sourcePath;
 
     public AttachRequestArguments(Map<String, Object> args) {
 
@@ -29,7 +33,6 @@ public class AttachRequestArguments {
         debuggerURL = localDebuggerURL;
 
         this.infobaseAlias = (String) args.get("infobaseAlias");
-        this.sessionId = (String) args.get("__sessionId");
 
         var passwordString = (String) args.get("password");
 
@@ -52,6 +55,29 @@ public class AttachRequestArguments {
 //        } else {
 //            targetTypes = new TargetType[]{};
 //        }
+
+        var args_cwd = (String) args.get("cwd");
+        if (Objects.isNull(args_cwd) || args_cwd.isBlank()) {
+            cwd = Paths.get("").toAbsolutePath().toString();
+        } else {
+            cwd = args_cwd;
+        }
+
+        Map<String, Object> sources = (Map<String, Object>) args.get("sources");
+        var args_sourcePath = "";
+        if (!Objects.isNull(sources)) {
+            args_sourcePath = (String) sources.get("path");
+        }
+
+        if (Objects.isNull(args_sourcePath) || args_sourcePath.isBlank()) {
+            args_sourcePath = "src/cf";
+        }
+
+        if (Path.of(args_sourcePath).isAbsolute()) {
+            sourcePath = args_sourcePath;
+        } else {
+            sourcePath = Path.of(cwd, args_sourcePath).toString();
+        }
     }
 
     public void validate(Language language) {
